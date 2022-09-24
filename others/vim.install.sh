a6936bb5a9963cc61f1f8b3d83c6bbbd912a63df
th2ch-g <th2ch-g>
2022-09-24 22:09:19 +0900

    revise installer

--- others/vim.install.sh
+++ others/vim.install.sh
@@ -4,13 +4,14 @@ set -e
 
 thread=15
 vim_link=https://github.com/vim/vim/archive/refs/tags/v9.0.0539.tar.gz
+install_path=$PWD
 
 curl -L $vim_link -o vim.tar.gz && \
     tar -zxvf vim.tar.gz && \
     rm -f vim.tar.gz
 
 cd vim* && \
-    ./configure --prefix=$PWD/vim --enable-perlinterp --enable-rubyinterp \
+    ./configure --prefix=${install_path}/vim --enable-perlinterp --enable-rubyinterp \
     --enable-multibyte --disable-netbeans --disable-gtktest --disable-acl \
     --disable-gpm --disable-xim --without-x --disable-gui && \
     make -j $thread && make install
--- others/zsh.install.sh
+++ others/zsh.install.sh
@@ -4,17 +4,15 @@ set -e
 
 thread=15
 zsh_link=https://github.com/zsh-users/zsh/archive/refs/tags/zsh-5.9.tar.gz
+install_path=$PWD
 
 curl -L $zsh_link -o zsh.tar.gz && \
     tar -zxvf zsh.tar.gz && \
     rm -f zsh.tar.gz
 
-
 cd zsh* && \
-    ./configure --prefix=$PWD/zsh \
+    ./configure --prefix=${install_path}/zsh \
     --enable-multibyte --enable-locale && \
     make -j $thread && make install
 
 echo "[INFO] zsh install done" >&1
-
-
