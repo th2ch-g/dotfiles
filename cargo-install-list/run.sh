2b199d0c245ea3519c6237cc5b204ac34beba39a
th2ch-g <th2ch-g>
2022-10-09 22:40:31 +0900

    revise cargo

--- cargo-install-list/run.sh
+++ cargo-install-list/run.sh
@@ -2,9 +2,9 @@
 cat list.txt | while IFS= read -r line;
 do
     if [[ $line =~ "https" ]]; then
-        cargo install --git $line
+        cargo install --locked --git $line
     else
-        cargo install $line
+        cargo install --locked $line
     fi
 done \
     && echo "[INFO] cargo-install-list done" >&1
