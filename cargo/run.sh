2803e399d437462c2775a205f61eb3336db48dd3
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-02-15 18:40:17 +0900

    add

--- cargo/run.sh
+++ cargo/run.sh
@@ -2,11 +2,12 @@
 cargo install cargo-quickinstall
 cat list.txt | while IFS= read -r line;
 do
-    [[ "$line" =~ "#" ]] && continue
-    if [[ $line =~ "https://" ]]; then
-        cargo install --locked --git $line
+    line_1=$(echo $line | awk '{print $1}')
+    [[ "$line_1" =~ "#" ]] && continue
+    if [[ $line_1 =~ "https://" ]]; then
+        cargo install --locked --git $line_1
     else
-        cargo quickinstall $line
+        cargo quickinstall $line_1
     fi
 done \
     && echo "[INFO] cargo-install-list done" >&1
