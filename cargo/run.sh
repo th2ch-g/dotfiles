334ca7c35ca80a4090b3344ac49e50ff627474ee
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-03-05 17:51:07 +0900

    add

--- cargo/run.sh
+++ cargo/run.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-cargo install cargo-quickinstall
+set -e
+
+echo "+ rustup self update" >&1
+rustup self update
+
+echo "+ rustup update" >&1
+rustup update
+
+echo "+ cargo-binstall" >&1
+# cargo install --locked --force cargo-quickinstall
+cargo install --locked --force cargo-binstall
+
 cat list.txt | while IFS= read -r line;
 do
     line_1=$(echo $line | awk '{print $1}')
+    line_2=$(echo $line | awk '{print $2}')
     [[ "$line_1" =~ "#" ]] && continue
+    echo "+ $line_1" >&1
     if [[ $line_1 =~ "https://" ]]; then
-        cargo install --locked --git $line_1
+        cargo install --locked --force --git $line_1 $line_2
+        # cargo binstall -y --force --locked --git $line_1 $line_2
     else
-        cargo quickinstall $line_1
+        # cargo quickinstall $line_1
+        cargo binstall -y --locked --force $line_1
     fi
 done \
     && echo "[INFO] cargo-install-list done" >&1
