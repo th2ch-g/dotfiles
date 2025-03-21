473e8fac156020e66807807a7cdb371e31ff17ce
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-03-21 23:27:23 +0900

    add

--- install.sh
+++ install.sh
@@ -273,6 +273,8 @@ if [ $python_flag -eq 0 ]; then
                 fi
             fi
             bash Miniconda3-latest-*-*.sh -p ${HOME}/works/tools/miniconda3 -b
+            rm -f Miniconda3-latest-*-*.sh
+            print_info "please run: conda init zsh"
         fi
         print_info "python install done"
     fi
