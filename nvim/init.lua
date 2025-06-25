aaaaf76c5f77750d2ebd166166ee22909d97c5ab
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-06-25 23:05:05 +0900

    Refactor: Update nvim configuration in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -149,8 +149,10 @@ if use_ai then
                     },
                 },
                 gemini = {
-                    -- model = "gemini-2.5-pro-exp-03-25",
-                    model = "gemini-2.5-flash-preview-04-17",
+                    -- https://ai.google.dev/gemini-api/docs/models?hl=ja#preview
+                    -- model = "gemini-2.5-pro-preview-06-05",
+                    -- model = "gemini-2.5-flash-preview-04-17",
+                    model = "gemini-2.5-flash-preview-05-20",
                     temperature = 0,
                     max_tokens = 4096,
                 },
