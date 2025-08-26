bb8d94c492157826ebbe10aaea314e69d28ddb85
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-08-26 14:14:21 +0900

    add lean

--- nvim/init.lua
+++ nvim/init.lua
@@ -101,7 +101,35 @@ local plugins = {
     --         })
     --     end,
     -- },
+
+
+    -- lean4
+    {
+        'Julian/lean.nvim',
+        event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+        dependencies = {
+          'neovim/nvim-lspconfig',
+          'nvim-lua/plenary.nvim',
+
+          -- optional dependencies:
+
+          -- a completion engine
+          --    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
+
+          -- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
+          -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
+          -- 'andrewradev/switch.vim',        -- for switch support
+          -- 'tomtom/tcomment_vim',           -- for commenting
+        },
+
+        ---@type lean.Config
+        opts = { -- see below for full configuration options
+          mappings = true,
+        }
+    },
 }
+
 
 local use_ai = vim.fn.getenv("VIM_AI") == "1"
 
