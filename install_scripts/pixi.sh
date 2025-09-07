70f65d880dcaaf53e55e34f1f122fd6666ced3ef
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-09-08 06:17:40 +0900

    fix pixi installation

--- install_scripts/pixi.sh
+++ install_scripts/pixi.sh
@@ -1,31 +1,35 @@
+#!/bin/bash
+set -e
 
-    if command -v pixi >/dev/null 2>&1; then
-        print_info "pixi is already installed"
-    else
-        print_error "pixi is not installed"
-        print_error "run first: ./install.sh --cargo"
-        print_error "or run:"
-        PIXI_VERSION="v0.50.2"
-        if [ $OS == "Mac" ]; then
-            if [[ $arch == "x86_64" || $arch == "amd64" ]]; then
-                print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-x86_64-apple-darwin.tar.gz"
-            elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-                print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-aarch64-apple-darwin.tar.gz"
-            else
-                print_error "Unknown architecture"
-                print_error "open https://github.com/prefix-dev/pixi/releases"
-                exit 1
-            fi
-        fi
-        if [ $OS == "Linux" ]; then
-            if [[ $arch == "x86_64" || $arch == "amd64" ]]; then
-                print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-x86_64-unknown-linux-musl.tar.gz"
-            elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-                print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-aarch64-unknown-linux-musl.tar.gz"
-            else
-                print_error "Unknown architecture"
-                print_error "open https://github.com/prefix-dev/pixi/releases"
-                exit 1
-            fi
+if command -v pixi >/dev/null 2>&1; then
+    print_info "pixi is already installed"
+else
+    print_error "pixi is not installed"
+    print_error "run first: ./install.sh --cargo"
+    print_error "or run:"
+    PIXI_VERSION="v0.50.2"
+    if [ $OS == "Mac" ]; then
+        if [[ $arch == "x86_64" || $arch == "amd64" ]]; then
+            print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-x86_64-apple-darwin.tar.gz"
+        elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+            print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-aarch64-apple-darwin.tar.gz"
+        else
+            print_error "Unknown architecture"
+            print_error "open https://github.com/prefix-dev/pixi/releases"
+            exit 1
         fi
     fi
+    if [ $OS == "Linux" ]; then
+        if [[ $arch == "x86_64" || $arch == "amd64" ]]; then
+            print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-x86_64-unknown-linux-musl.tar.gz"
+        elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+            print_error "wget https://github.com/prefix-dev/pixi/releases/download/${PIXI_VERSION}/pixi-aarch64-unknown-linux-musl.tar.gz"
+        else
+            print_error "Unknown architecture"
+            print_error "open https://github.com/prefix-dev/pixi/releases"
+            exit 1
+        fi
+    fi
+fi
+
+echo done
