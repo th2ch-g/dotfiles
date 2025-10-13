b0f7fbe1b0f8f44332cc935a4accbf1218f61d0b
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-10-13 12:59:36 +0900

    add new mcp

--- install_scripts/gemini-cli.sh
+++ install_scripts/gemini-cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-URL="https://github.com/google-gemini/gemini-cli/releases/download/v0.6.0-preview.3/gemini.js"
+URL="https://github.com/google-gemini/gemini-cli/releases/download/v0.9.0-preview.2/gemini.js"
 wget $URL
 chmod a+x gemini.js
 mv gemini.js gemini
