61190aac3130a6134d5bab3adaaf559f55fd7471
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-11-10 00:11:39 +0900

    update install scripts

--- install_scripts/prezto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eux
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-echo done
--- install_scripts/zsh.sh
+++ install_scripts/zsh.sh
@@ -3,14 +3,14 @@ set -eux
 
 thread=15
 VERSION="5.9"
-PREFIX="${PWD}/zsh-${VERSION}/build"
+PREFIX="${PWD}/zsh-zsh-${VERSION}/build"
 
 URL=https://github.com/zsh-users/zsh/archive/refs/tags/zsh-${VERSION}.tar.gz
 curl -L $URL -o zsh.tar.gz && \
     tar -zxvf zsh.tar.gz && \
     rm -f zsh.tar.gz
 
-cd zsh-${VERSION} && \
+cd zsh-zsh-${VERSION} && \
     ./Util/preconfig && \
     ./configure --prefix=${PREFIX} \
     --enable-multibyte --enable-locale && \
