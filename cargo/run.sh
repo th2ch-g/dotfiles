ea99438a659337519652141ab136d374bd9518bf
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2026-01-26 03:48:36 +0900

    add

--- cargo/run.sh
+++ cargo/run.sh
@@ -14,11 +14,17 @@ do
     [[ "$line_1" =~ "#" ]] && continue
     echo "+ $line_1" >&1
     if [[ $line_1 =~ "https://" ]]; then
-        cargo install --locked --force --git $line_1 $line_2
-        # cargo binstall -y --force --locked --git $line_1 $line_2
+        if [[ $line_2 =~ "#" ]]; then
+            cargo install --locked --force --git $line_1
+            # cargo binstall -y --force --locked --git $line_1 $line_2
+        else
+            cargo install --locked --force --git $line_1 $line_2
+            # cargo binstall -y --force --locked --git $line_1 $line_2
+        fi
     else
         # cargo quickinstall $line_1
         cargo binstall -y --locked --force $line_1
     fi
 done \
     && echo "[INFO] cargo-install-list done" >&1
+
